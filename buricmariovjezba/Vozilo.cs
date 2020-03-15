7878e67ef14c39c13bd36ad1d511a61f34f14771
MarioBuric <55134593+MarioBuric@users.noreply.github.com>
2020-03-15 11:20:18 +0100

    Add files via upload

---
+++ buricmariovjezba/Vozilo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buricmariovjezba
+{
+    class Vozilo
+    {
+        private string vrsta; 
+        private double maxBrzina;
+        private int brojKotaca;
+        public Vozilo()
+        { }
+        public void setVrsta(string vrsta)
+        {
+            this.vrsta = vrsta;
+        }
+        public string getVrsta()
+        {
+            return vrsta;
+        }
+        public void setMaxBrzina(double maxBrzina)
+        {
+            this.maxBrzina = maxBrzina;
+        }
+        public double getMaxBrzina()
+        {
+            return maxBrzina;
+        }
+        public void setBrojKotaca(int brojKotaca)
+        {
+            this.brojKotaca = brojKotaca;
+        }
+        public int getBrojKotaca()
+        {
+            return brojKotaca;
+        }
+        public override string ToString()
+        {
+            string poruka = "Vrsta: " + vrsta +
+                "\nMaksinalna brzina: " + maxBrzina +
+                "\nBroj kotača: " + brojKotaca;
+            return poruka;
+        }
+    }
+}
